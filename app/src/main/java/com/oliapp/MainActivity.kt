74c9c9aa077e1c64a46855bb6aa5ff44681c22ab
Ck Will <chwilliams@epsilonsystems.com>
2021-05-04 02:50:12 -0700

    Bottom Nav Bar added.

--- app/src/main/java/com/oliapp/MainActivity.kt
+++ app/src/main/java/com/oliapp/MainActivity.kt
@@ -3,6 +3,12 @@ package com.oliapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,15 +19,22 @@ class MainActivity : AppCompatActivity() {
 
         val userID = intent.getStringExtra("userID")
         val emailID = intent.getStringExtra("emailID")
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
 
-        mainUserID.text = "User ID: $userID"
-        mainEmailID.text = "Email ID: $emailID"
+        val appbarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment, R.id.fourthFragment))
+        setupActionBarWithNavController(navController, appbarConfiguration)
 
-        mainBTNLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
+        bottomNavigationView.setupWithNavController(navController)
 
-            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-            finish()
-        }
+        //mainUserID.text = "User ID: $userID"
+       // mainEmailID.text = "Email ID: $emailID"
+
+//        mainBTNLogout.setOnClickListener {
+//            FirebaseAuth.getInstance().signOut()
+//
+//            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+//            finish()
+//        }
     }
 }
